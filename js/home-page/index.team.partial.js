def83dd7d0bc443cd8a79fa91fa11c8819899a8b
Oleksandr Kozynets <kozynetsoleksandr8@gmail.com>
2025-06-12 13:23:04 +0100

    add section course-details and fix  Sliders

--- js/home-page/index.team.partial.js
+++ js/home-page/index.team.partial.js
@@ -1,10 +1,14 @@
 import { Slider } from "../components/Slider.js";
 
-const mySlider = new Slider({
-  container: document.querySelector(".team__slider-container"),
-  track: document.querySelector(".team__slider-track"),
-  btnPrev: document.querySelector(".team__nav-btn--prev"),
-  btnNext: document.querySelector(".team__nav-btn--next"),
-  slidesToShow: 4,
-  slidesToScroll: 3,
-});
+const teamContainer = document.querySelector(".team__slider-container");
+
+if (teamContainer) {
+  new Slider({
+    container: document.querySelector(".team__slider-container"),
+    track: document.querySelector(".team__slider-track"),
+    btnPrev: document.querySelector(".team__nav-btn--prev"),
+    btnNext: document.querySelector(".team__nav-btn--next"),
+    slidesToShow: 4,
+    slidesToScroll: 3,
+  });
+}
--- js/home-page/index.testimonials.partial.js
+++ js/home-page/index.testimonials.partial.js
@@ -1,10 +1,14 @@
 import { Slider } from "../components/Slider.js";
 
-const mySlider = new Slider({
-  container: document.querySelector(".testimoinals__slider-container"),
-  track: document.querySelector(".testimoinals__slider-track"),
-  btnPrev: document.querySelector(".testimoinals__nav-btn--prev"),
-  btnNext: document.querySelector(".testimoinals__nav-btn--next"),
-  slidesToShow: 1,
-  slidesToScroll: 1,
-});
+const testimonialsContainer = document.querySelector(".testimoinals__slider-container");
+
+if (testimonialsContainer) {
+  new Slider({
+    container: testimonialsContainer,
+    track: document.querySelector(".testimoinals__slider-track"),
+    btnPrev: document.querySelector(".testimoinals__nav-btn--prev"),
+    btnNext: document.querySelector(".testimoinals__nav-btn--next"),
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  })
+}
